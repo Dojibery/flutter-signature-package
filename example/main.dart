027c0c2036b2b8ba70ff95d2cdff908092256fab
vladislavenev <vlladislav45@gmail.com>
2024-07-10 10:33:59 +0300

    fix: imports

--- example/main.dart
+++ example/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:signature_package/flutter_signature_package.dart';
+import 'package:flutter_signature_package/flutter_signature_package.dart';
 import 'package:provider/provider.dart';
 
 void main() {
--- test/signature_package_test.dart
+++ test/signature_package_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:signature_package/flutter_signature_package.dart';
+import 'package:flutter_signature_package/flutter_signature_package.dart';
 
 void main() {
   // test('adds one to input values', () {
